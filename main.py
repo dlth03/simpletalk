ae2fd5fdd66fedbf95de54cdf845428cf41a8afc
dlth03 <127368674+dlth03@users.noreply.github.com>
2025-06-02 13:30:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -305,4 +305,3 @@ async def translate_to_easy_korean(input_data: TextInput):
     except Exception as e:
         print(f"[translate-to-easy-korean] API 처리 중 에러: {e}")
         raise HTTPException(status_code=500, detail=f"API 처리 중 에러가 발생했습니다: {e}")
-main
